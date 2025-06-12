163b230762c4c73ee2f2dc496618854f7532373b
Neha Bharti <neetneha116@gmail.com>
2025-06-12 11:58:28 +0530

    first commit

---
+++ src/test/java/com/crud/project/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.crud.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+
+	}
+
+}
